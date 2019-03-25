4e4dd0c5e8ddc0819a2857e9ee0789cc24b7c0f1
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-03-25 15:07:22 -0700

    Misc fixes and ports (#43)
    * Ported some changes and fixes.
    Includes:
    - updates to the glTF utility
    - misc fixes to timing of transforms for pointers
    
    * revert to pointer late updates with an abstract override instead.
    
    * ported more change requests from glTF review
    
    * updated ignore path to include glTF demo model exclusion from repository
    
    * Ported gaze provider stability changes.
    Ported raycast sorting fixes.
    
    * updated streaming assets ignore file and updated the custom services docs

--- Utilities/Lines/Renderers/MixedRealityLineRenderer.cs
+++ Utilities/Lines/Renderers/MixedRealityLineRenderer.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.Extensions;
 using UnityEngine;
 using UnityEngine.Rendering;
 using XRTK.Definitions.Lines;
+using XRTK.Extensions;
 using XRTK.Utilities.Lines.DataProviders;
 
 namespace XRTK.Utilities.Lines.Renderers
@@ -68,7 +68,7 @@ namespace XRTK.Utilities.Lines.Renderers
             }
         }
 
-        private void LateUpdate()
+        protected override void UpdateLine()
         {
             if (LineDataSource == null)
             {

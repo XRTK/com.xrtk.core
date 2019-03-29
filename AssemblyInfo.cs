428befd1f8e43c16a0727d0c4d1c6f09b38863ff
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-03-29 10:15:11 -0700

    Removed the AsyncCoroutineRunner as it was a singleton and updated the async runner to now start the coroutines on the MixedRealityToolkit itself. (#53)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,5 +5,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("XRTK.Inspectors")]
 [assembly: InternalsVisibleTo("XRTK.Tests")]
+[assembly: InternalsVisibleTo("XRTK.Utilities.Gltf.Importers")]
 [assembly: InternalsVisibleTo("XRTK.WindowsMixedReality")]
 [assembly: InternalsVisibleTo("XRTK.WindowsMixedReality.Player")]

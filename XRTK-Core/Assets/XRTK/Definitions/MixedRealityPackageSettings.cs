2f9ba4b19cd1987ae8d477904eedd8a03164c8d3
Stephen Hodgson <hodgson.designs@gmail.com>
2019-04-02 09:35:26 -0700

    removed extra components for upm and updated editor to 2018.3.11f1

---
+++ XRTK-Core/Assets/XRTK/Definitions/MixedRealityPackageSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace XRTK.Definitions
+{
+    /// <summary>
+    /// Data container for holding the settings for the upm packages associated with the XRTK.
+    /// </summary>
+    public class MixedRealityPackageSettings : ScriptableObject
+    {
+        [SerializeField]
+        private string[] packageNames = new string[0];
+
+        [SerializeField]
+        private string[] packageUrls = new string[0];
+
+        public Dictionary<string, string> PackageList
+        {
+            get
+            {
+                packageList.Clear();
+
+                for (int i = 0; i < packageNames.Length; i++)
+                {
+                    packageList.Add(packageNames[i], packageUrls[i]);
+                }
+
+                return packageList;
+            }
+        }
+
+        private Dictionary<string, string> packageList = new Dictionary<string, string>(0);
+    }
+}

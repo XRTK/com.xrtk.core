a40426e222299ad836ca73ef29e393e935afae5b
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-05-20 16:12:30 -0700

    Find common root for GameObject and Transforms (#197)

--- Extensions/GameObjectExtensions.cs
+++ Extensions/GameObjectExtensions.cs
@@ -172,5 +172,39 @@ namespace XRTK.Extensions
                 }
             }
         }
+
+        /// <summary>
+        /// Given 2 GameObjects, return a common root GameObject (or null).
+        /// </summary>
+        /// <param name="g1">GameObject to compare</param>
+        /// <param name="g2">GameObject to compare</param>
+        public static GameObject FindCommonRoot(this GameObject g1, GameObject g2)
+        {
+            if (g1 == null || g2 == null)
+            {
+                return null;
+            }
+
+            var t1 = g1.transform;
+
+            while (t1 != null)
+            {
+                var t2 = g2.transform;
+
+                while (t2 != null)
+                {
+                    if (t1 == t2)
+                    {
+                        return t1.gameObject;
+                    }
+
+                    t2 = t2.parent;
+                }
+
+                t1 = t1.parent;
+            }
+
+            return null;
+        }
     }
 }

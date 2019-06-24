aa22b3e302570448a20ee042bafafb977a71df35
Dino Fejzagic <dino.f@live.de>
2019-06-24 22:30:47 +0200

    Define camera simulation data provider

---
+++ XRTK-Core/Packages/com.xrtk.core/Providers/InputSystem/Simulation/BaseSimulationDataProvider.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.Providers.InputSystem.Simulation;
+using XRTK.Services;
+
+namespace XRTK.Providers.InputSystem.Simulation
+{
+    public class BaseSimulationDataProvider : BaseDataProvider, IMixedRealityInputSimulationDataProvider
+    {
+        public BaseSimulationDataProvider(string name, uint priority) : base(name, priority)
+        {
+        }
+    }
+}
---
+++ XRTK-Core/Packages/com.xrtk.core/Providers/InputSystem/Simulation/CameraSimulationDataProvider.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace XRTK.Providers.InputSystem.Simulation
+{
+    public class CameraSimulationDataProvider : BaseSimulationDataProvider
+    {
+        public CameraSimulationDataProvider(string name, uint priority) : base(name, priority)
+        {
+        }
+    }
+}

2dda6d66ece24d4fc4840a5eea4b1961e09fa75b
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-08-13 14:50:57 -0400

    Fixed controller namespaces (#284)
    * Fixed controller namespaces
    
    * updated SDK checkout
    
    * Changed a few classes to structs and updated the sdk checkout
    
    * revert some changes to correct namespaces that were broken
    
    * forgotten the lumin package in the last push
    
    * Update XRTK-Core/Packages/com.xrtk.core/Providers/Controllers/MixedRealityControllerMapping.cs
    
    * fixed missing references
    
    * reverted mapping type change from struct back to class
    
    * Fixed profile inspector folder path

--- XRTK-Core/Packages/com.xrtk.core/Providers/Controllers/BaseDictationDataProvider.cs
+++ XRTK-Core/Packages/com.xrtk.core/Providers/Controllers/BaseDictationDataProvider.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
 using UnityEngine;
+using XRTK.Definitions.Controllers;
 using XRTK.Interfaces.Providers.Controllers;
 
 namespace XRTK.Providers.Controllers

dd57d7a18dc8dfa0c2755ccffdf1dc0cc5394b50
Dino Fejzagic <dino.f@live.de>
2019-08-15 20:19:33 +0200

    Add hand controller provider configuration

--- XRTK-Core/Packages/com.xrtk.core/Definitions/Controllers/Hands/HandControllerDataProviderProfile.cs
+++ XRTK-Core/Packages/com.xrtk.core/Definitions/Controllers/Hands/HandControllerDataProviderProfile.cs
@@ -9,6 +9,12 @@ namespace XRTK.Definitions.Controllers.Hands
     [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Input System/Controller Data Providers/Hands", fileName = "MixedRealityHandControllerDataProviderProfile", order = (int)CreateProfileMenuItemIndices.Input)]
     public class HandControllerDataProviderProfile : BaseMixedRealityControllerDataProviderProfile
     {
+        [SerializeField]
+        private HandControllerDataProviderConfiguration[] registeredControllerDataProviders = new HandControllerDataProviderConfiguration[0];
 
+        /// <summary>
+        /// The currently registered controller data providers for this input system.
+        /// </summary>
+        public HandControllerDataProviderConfiguration[] RegisteredControllerDataProviders => registeredControllerDataProviders;
     }
 }

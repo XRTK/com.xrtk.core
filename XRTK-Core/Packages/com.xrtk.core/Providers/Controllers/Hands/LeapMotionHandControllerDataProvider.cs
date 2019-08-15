a87e0c0132ab4e34fe6700ac2db20e25b795a500
Dino Fejzagic <dino.f@live.de>
2019-08-15 20:33:49 +0200

    Add leap motion hand controller data provider

---
+++ XRTK-Core/Packages/com.xrtk.core/Providers/Controllers/Hands/LeapMotionHandControllerDataProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.Providers.Controllers;
+using XRTK.Services;
+
+namespace XRTK.Providers.Controllers.Hands
+{
+    public class LeapMotionHandControllerDataProvider : BaseDataProvider, IMixedRealityPlatformHandControllerDataProvider
+    {
+        public LeapMotionHandControllerDataProvider(string name, uint priority) : base(name, priority)
+        {
+        }
+
+        public IMixedRealityController[] GetActiveControllers()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

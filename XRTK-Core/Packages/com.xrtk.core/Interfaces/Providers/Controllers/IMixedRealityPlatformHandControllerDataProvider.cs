d3cdb8c184ac8ce6ac307bccde527de1f9dede3a
Dino Fejzagic <dino.f@live.de>
2019-08-15 20:19:55 +0200

    Implement inspector

---
+++ XRTK-Core/Packages/com.xrtk.core/Interfaces/Providers/Controllers/IMixedRealityPlatformHandControllerDataProvider.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace XRTK.Interfaces.Providers.Controllers
+{
+    public interface IMixedRealityPlatformHandControllerDataProvider : IMixedRealityControllerDataProvider
+    {
+
+    }
+}

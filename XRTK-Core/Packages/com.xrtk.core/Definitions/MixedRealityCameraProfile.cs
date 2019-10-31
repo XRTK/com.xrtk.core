69da7ecdda0c536259d5ba599cd57c2e8d9cc20c
Dino Fejzagic <dino.f@live.de>
2019-10-31 16:42:08 +0100

    Merge cleanup

--- XRTK-Core/Packages/com.xrtk.core/Definitions/MixedRealityCameraProfile.cs
+++ XRTK-Core/Packages/com.xrtk.core/Definitions/MixedRealityCameraProfile.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
-using XRTK.Definitions.Devices;
 using XRTK.Attributes;
 using XRTK.Definitions.Utilities;
 using XRTK.Interfaces;

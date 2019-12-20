db216ed1e7ed699efc0ad5e8dbae2e54649c0479
Dino Fejzagic <dino.f@live.de>
2019-12-20 17:46:50 +0100

    Define diagnostics handler interface

---
+++ XRTK-Core/Packages/com.xrtk.core/Interfaces/Diagnostics/IMixedRealityDiagnosticsHandler.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Services.DiagnosticsSystem;
+
+namespace XRTK.Interfaces.Diagnostics
+{
+    public interface IMixedRealityDiagnosticsHandler
+    {
+        /// <summary>
+        /// Updates the visuailzed diagnostics.
+        /// </summary>
+        /// <param name="data">The new diagnostics data.</param>
+        void UpdateDiagnostics(DiagnosticsData data);
+    }
+}

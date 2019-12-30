e07db794d93370e0d1af3f22a49312a830f91b5d
Dino Fejzagic <dino.f@live.de>
2019-12-30 15:08:09 +0100

    Feature/diagnostics system console (#422)
    * Recategorize existing diagnostics settings in profile and inspector
    
    * Move diagnostics visualization settings from MonoBehaviour to profile
    
    * Fix namings and create new console visuailzer component
    
    * Refactor diagnostics system to a more XRTK conforming architecture
    
    * Rename visualizers
    
    * Add visuailzation prefab setting to profile
    
    * Define diagnostics handler interface
    
    * Add diagnosteics transform to diag system definition
    
    * Define diagnostics data (draft)
    
    * Implement base diagnostics handler
    
    * Instantiate configured visuailzer
    
    * Start work on Canvas based diagnostics visualizer
    
    * Add TextMeshPro to XRTK.Core assembly references
    
    * Remove diagnostics data
    
    * Implement base diagnostics data provider
    
    * Move visualization prefab to diag sys profile
    
    * Update interfaces
    
    * Update base implementation of handler
    
    * Implement memory diagnostics data provider
    
    * Implement console diagnostics data provider
    
    * Update diagnostics system interface with common diag
    
    * Implement frame diagnostics  data provider
    
    * Implement option to show or hide diagnostics on start
    
    * Fix diagnostics namespaces
    
    * Implement handlers for default diagnostics
    
    * Remove unnecessary stopwatch reset when intance is destroyed
    
    * Remove static fields in implementation
    
    * Update submoduule
    
    * Use typed profile for diag data provider configuration
    
    * updated sdk checkout
    
    * updated sdk checkout
    
    * Change requests/diagnostics system update (#426)
    
    * updated diagnostics system with appropriate event datum and event routing
    
    * updated sdk
    
    * updated sdk components
    
    * Diagnostics events
    
    * updated oculus sdk
    
    * updated sdk checkout
    
    * a bit of cleanup
    
    * updated sdk checkout
    
    * updated sdk checkout
    
    * updated submodule checkout
    
    * a bit of organization in the diagnostics system interface and implementation
    
    * updated sdk
    
    * Update submodule
    
    Co-authored-by: Stephen Hodgson <StephenHodgson@users.noreply.github.com>

---
+++ XRTK-Core/Packages/com.xrtk.core/Interfaces/DiagnosticsSystem/Handlers/IMixedRealityDiagnosticsHandler.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine.EventSystems;
+
+namespace XRTK.Interfaces.DiagnosticsSystem.Handlers
+{
+    public interface IMixedRealityDiagnosticsHandler : IEventSystemHandler { }
+}
---
+++ XRTK-Core/Packages/com.xrtk.core/Interfaces/DiagnosticsSystem/IMixedRealityGenericDiagnosticsDataProvider.cs
@@ -0,0 +1,22 @@
+﻿//// Copyright (c) XRTK. All rights reserved.
+//// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+//using XRTK.Interfaces.DiagnosticsSystem.Handlers;
+
+//namespace XRTK.Interfaces.DiagnosticsSystem
+//{
+//    public interface IMixedRealityGenericDiagnosticsDataProvider<T> : IMixedRealityDiagnosticsDataProvider where T : IMixedRealityDiagnosticsHandler
+//    {
+//        /// <summary>
+//        /// Registers the diagnostics handler to receive updates.
+//        /// </summary>
+//        /// <param name="handler">The handler to register.</param>
+//        void Register(T handler);
+
+//        /// <summary>
+//        /// Unregisters the diagnostics handler from receiving updates.
+//        /// </summary>
+//        /// <param name="handler">The handler to unregister.</param>
+//        void Unregister(T handler);
+//    }
+//}

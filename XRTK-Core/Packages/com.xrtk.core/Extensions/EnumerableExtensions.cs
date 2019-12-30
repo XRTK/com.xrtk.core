8f300b7ad882c210b6496b98226297134149fb69
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-12-30 08:23:49 -0500

    broke out the changes from the diagnostics PR (#428)
    * broke out the changes from the diagnostics PR
    
    * updated xrtk-core package to 0.1.28

--- XRTK-Core/Packages/com.xrtk.core/Extensions/EnumerableExtensions.cs
+++ XRTK-Core/Packages/com.xrtk.core/Extensions/EnumerableExtensions.cs
@@ -17,7 +17,7 @@ namespace XRTK.Extensions
         /// <returns>Max or default value of T</returns>
         public static T MaxOrDefault<T>(this IEnumerable<T> items, IComparer<T> comparer = null)
         {
-            if (items == null) { throw new ArgumentNullException("items"); }
+            if (items == null) { throw new ArgumentNullException(nameof(items)); }
             comparer = comparer ?? Comparer<T>.Default;
 
             using (var enumerator = items.GetEnumerator())

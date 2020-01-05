ca34b25edda221825100e54e9352ada9e279087c
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-01-05 11:58:58 +0000

    Editor folder clean up, removed junk class and added license info to IconEditor class

--- XRTK-Core/Packages/com.xrtk.core/Utilities/Editor/PathFinder.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace XRTK.Utilities.Editor
-{
-    /// <summary>
-    /// Dummy scriptable object used to find the relative path of the editor.
-    /// </summary>
-    internal class PathFinder : ScriptableObject { }
-}

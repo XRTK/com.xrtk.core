aeb9f74bd402da0d91eebcb86acb16d5b56c0064
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-01-05 12:08:57 +0000

    Reverting PathFinder deletion and added license

---
+++ XRTK-Core/Packages/com.xrtk.core/Utilities/Editor/PathFinder.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace XRTK.Utilities.Editor
+{
+    /// <summary>
+    /// Dummy scriptable object used to find the relative path of the editor.
+    /// </summary>
+    internal class PathFinder : ScriptableObject { }
+}

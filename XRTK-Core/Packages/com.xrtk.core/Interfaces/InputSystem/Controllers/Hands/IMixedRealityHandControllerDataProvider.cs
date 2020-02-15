51b06b5ee22cbbac6a4e5ff92b4d804c6e890a36
Dino Fejzagic <dino.f@live.de>
2020-02-15 14:37:26 +0100

    Introduce base hand controller data provide to consume shared profile

---
+++ XRTK-Core/Packages/com.xrtk.core/Interfaces/InputSystem/Controllers/Hands/IMixedRealityHandControllerDataProvider.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.Providers.Controllers;
+using XRTK.Providers.Controllers.Hands;
+
+namespace XRTK.Interfaces.InputSystem
+{
+    public interface IMixedRealityHandControllerDataProvider : IMixedRealityControllerDataProvider
+    {
+        /// <summary>
+        /// Are hand physics enabled?
+        /// </summary>
+        bool HandPhysicsEnabled { get; }
+
+        /// <summary>
+        /// Shall hand colliders be triggers?
+        /// </summary>
+        bool UseTriggers { get; }
+
+        /// <summary>
+        /// Gets the configured hand bounds mode to be used with hand physics.
+        /// </summary>
+        HandBoundsMode BoundsMode { get; }
+    }
+}
---
+++ XRTK-Core/Packages/com.xrtk.core/Providers/Controllers/Hands/BaseHandControllerDataProvider.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Definitions.Controllers.Hands;
+using XRTK.Interfaces.InputSystem;
+
+namespace XRTK.Providers.Controllers.Hands
+{
+    /// <summary>
+    /// Base implementation with shared / common elements between all supported hand controllers.
+    /// </summary>
+    public abstract class BaseHandControllerDataProvider<T> : BaseControllerDataProvider, IMixedRealityHandControllerDataProvider where T : MixedRealityHandControllerDataProviderProfile
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Name of the data provider as assigned in the configuration profile.</param>
+        /// <param name="priority">Data provider priority controls the order in the service registry.</param>
+        /// <param name="profile">Controller data provider profile assigned to the provider instance in the configuration inspector.</param>
+        public BaseHandControllerDataProvider(string name, uint priority, T profile)
+            : base(name, priority, profile)
+        {
+            Profile = profile;
+        }
+
+        /// <summary>
+        /// Gets the active hand controller data provider profile.
+        /// </summary>
+        protected T Profile { get; }
+
+        /// <inheritdoc />
+        public bool HandPhysicsEnabled { get; private set; }
+
+        /// <inheritdoc />
+        public bool UseTriggers { get; private set; }
+
+        /// <inheritdoc />
+        public HandBoundsMode BoundsMode { get; private set; }
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            HandPhysicsEnabled = Profile.HandPhysicsEnabled;
+            UseTriggers = Profile.UseTriggers;
+            BoundsMode = Profile.BoundsMode;
+        }
+    }
+}

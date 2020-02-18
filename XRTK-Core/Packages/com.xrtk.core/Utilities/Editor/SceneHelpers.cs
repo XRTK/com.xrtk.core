292c0b1ebb7467901a3a4a19296f03939456cbf5
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-02-18 09:16:49 +0000

    Transferring new SceneHelper script to core

---
+++ XRTK-Core/Packages/com.xrtk.core/Utilities/Editor/SceneHelpers.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEngine;
+
+namespace XRTK.Utilities.Editor
+{
+    public class SceneHelpers
+    {
+        /// <summary>
+        /// Simple scene helper to create the beginnings of a scene, creating the scene root and a floor.
+        /// </summary>
+        [MenuItem("Mixed Reality Toolkit/Tools/Create Floor", false, 1)]
+        public static void CreateFloor()
+        {
+            //check if there is already a Scene Objects GO
+            var sceneRoot = GameObject.Find("Scene Objects");
+            if(!sceneRoot)
+            {
+                sceneRoot = new GameObject("Scene Objects");
+                var floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                floor.name = "Ground";
+                floor.transform.SetParent(sceneRoot.transform);
+            }
+        }
+    }
+}

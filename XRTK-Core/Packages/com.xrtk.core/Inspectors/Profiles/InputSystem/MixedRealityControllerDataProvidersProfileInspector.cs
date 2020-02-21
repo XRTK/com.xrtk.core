0ef9e5349ac4199726786ae350936f39a8131d28
Stephen Hodgson <hodgson.designs@gmail.com>
2020-02-21 12:11:22 -0500

    use the proper base property for Configurations

--- XRTK-Core/Packages/com.xrtk.core/Inspectors/Profiles/InputSystem/MixedRealityControllerDataProvidersProfileInspector.cs
+++ XRTK-Core/Packages/com.xrtk.core/Inspectors/Profiles/InputSystem/MixedRealityControllerDataProvidersProfileInspector.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
 using UnityEditor;
@@ -10,21 +10,19 @@ using XRTK.Services;
 namespace XRTK.Inspectors.Profiles.InputSystem
 {
     [CustomEditor(typeof(MixedRealityControllerDataProvidersProfile))]
-    public class MixedRealityControllerDataProvidersProfileInspector : BaseMixedRealityProfileInspector
+    public class MixedRealityControllerDataProvidersProfileInspector : MixedRealityServiceProfileInspector
     {
         private static readonly GUIContent AddControllerDataProviderContent = new GUIContent("+ Add a New Controller Data Provider");
         private static readonly GUIContent RemoveControllerDataProviderContent = new GUIContent("-", "Remove Controller Data Provider");
         private static readonly GUIContent ProfileContent = new GUIContent("Profile");
 
-        private SerializedProperty configurations;
         private bool[] foldouts = null;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
-            configurations = serializedObject.FindProperty(nameof(configurations));
-            foldouts = new bool[configurations.arraySize];
+            foldouts = new bool[Configurations.arraySize];
         }
 
         public override void OnInspectorGUI()
@@ -51,8 +49,8 @@ namespace XRTK.Inspectors.Profiles.InputSystem
 
             if (GUILayout.Button(AddControllerDataProviderContent, EditorStyles.miniButton))
             {
-                configurations.arraySize += 1;
-                var newConfiguration = configurations.GetArrayElementAtIndex(configurations.arraySize - 1);
+                Configurations.arraySize += 1;
+                var newConfiguration = Configurations.GetArrayElementAtIndex(Configurations.arraySize - 1);
                 var dataProviderType = newConfiguration.FindPropertyRelative("instancedType");
                 var dataProviderName = newConfiguration.FindPropertyRelative("name");
                 var priority = newConfiguration.FindPropertyRelative("priority");
@@ -66,16 +64,16 @@ namespace XRTK.Inspectors.Profiles.InputSystem
                 runtimePlatform.intValue = 0;
                 profile.objectReferenceValue = null;
                 serializedObject.ApplyModifiedProperties();
-                foldouts = new bool[configurations.arraySize];
+                foldouts = new bool[Configurations.arraySize];
                 changed = true;
             }
 
             EditorGUILayout.Space();
             EditorGUILayout.BeginVertical();
 
-            for (int i = 0; i < configurations.arraySize; i++)
+            for (int i = 0; i < Configurations.arraySize; i++)
             {
-                var controllerConfiguration = configurations.GetArrayElementAtIndex(i);
+                var controllerConfiguration = Configurations.GetArrayElementAtIndex(i);
                 var dataProviderName = controllerConfiguration.FindPropertyRelative("name");
                 var dataProviderType = controllerConfiguration.FindPropertyRelative("instancedType");
                 var priority = controllerConfiguration.FindPropertyRelative("priority");
@@ -87,9 +85,9 @@ namespace XRTK.Inspectors.Profiles.InputSystem
 
                 if (GUILayout.Button(RemoveControllerDataProviderContent, EditorStyles.miniButtonRight, GUILayout.Width(24f)))
                 {
-                    configurations.DeleteArrayElementAtIndex(i);
+                    Configurations.DeleteArrayElementAtIndex(i);
                     serializedObject.ApplyModifiedProperties();
-                    foldouts = new bool[configurations.arraySize];
+                    foldouts = new bool[Configurations.arraySize];
                     EditorGUILayout.EndHorizontal();
                     EditorGUILayout.EndVertical();
                     return;

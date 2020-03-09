595feec69b8a6378d3bdae28f4d63b290cc328d3
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-03-09 16:31:23 +0000

    * Added new "InitialiseArray" array extension to set all values of an array to a new value, including Tests * Added null check on Array AddItem Fixed license in Core test 2

---
+++ XRTK-Core/Assets/XRTK.Tests/Core/TestFixture_03_ExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using NUnit.Framework;
+using UnityEngine;
+using XRTK.Extensions;
+
+namespace XRTK.Tests.Core
+{
+    public class TestFixture_03_ExtensionsTests
+    {
+        [Test]
+        public void Test_01_Array_AddItem_Extension()
+        {
+            Vector2[] testArray = null;
+            Vector2[] newArray =  null;
+            try
+            {
+                newArray = testArray.AddItem(Vector2.zero);
+            }
+            catch { }
+
+            Assert.IsNotNull(newArray);
+
+            testArray = new Vector2[5];
+            newArray = testArray.AddItem(Vector2.one);
+
+            Assert.IsTrue(newArray[newArray.Length - 1] == Vector2.one);
+            Assert.IsTrue(newArray.Length > testArray.Length);
+            Assert.IsTrue(newArray.Length - testArray.Length == 1);
+        }
+
+        [Test]
+        public void Test_02_Array_InitialiseArray_Extension()
+        {
+            Vector2[] testArray = null;
+            Vector2[] newArray = null;
+            try
+            {
+                newArray = testArray.InitialiseArray(Vector2.one);
+            }
+            catch { }
+
+            Assert.IsNotNull(newArray);
+            Assert.IsTrue(newArray.Length == 1);
+            Assert.IsTrue(newArray[0] == Vector2.one);
+
+            testArray = new Vector2[5];
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                testArray[i] = Vector2.one;
+            }
+            newArray = testArray.InitialiseArray(Vector2.zero);
+
+            Assert.IsTrue(newArray[0] == Vector2.zero);
+        }
+    }
+}

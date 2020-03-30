1caf320a4f21f03ad24cd93078fb88721ad0dcf3
Stephen Hodgson <hodgson.designs@gmail.com>
2020-03-30 16:51:22 -0400

    updated simulated hand data profile with required fields for IMixedRealityHandControllerDataProvider

--- XRTK-Core/Packages/com.xrtk.core/Definitions/Controllers/Simulation/Hands/SimulatedHandControllerDataProviderProfile.cs
+++ XRTK-Core/Packages/com.xrtk.core/Definitions/Controllers/Simulation/Hands/SimulatedHandControllerDataProviderProfile.cs
@@ -3,7 +3,10 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using XRTK.Attributes;
 using XRTK.Definitions.Utilities;
+using XRTK.Interfaces.InputSystem.Controllers.Hands;
+using XRTK.Providers.Controllers.Hands;
 
 namespace XRTK.Definitions.Controllers.Simulation.Hands
 {
@@ -27,5 +30,56 @@ namespace XRTK.Definitions.Controllers.Simulation.Hands
         /// Pose interpolation per second.
         /// </summary>
         public float HandPoseAnimationSpeed => handPoseAnimationSpeed;
+
+
+        [Header("General Settings")]
+
+        [SerializeField]
+        [Tooltip("If set, hand mesh data will be read and available for visualization. Disable for optimized performance.")]
+        private bool handMeshingEnabled = false;
+
+        /// <summary>
+        /// If set, hand mesh data will be read and available for visualization. Disable for optimized performance.
+        /// </summary>
+        public bool HandMeshingEnabled => handMeshingEnabled;
+
+        [SerializeField]
+        [Tooltip("The hand ray concrete type to use when raycasting for hand interaction.")]
+        [Implements(typeof(IMixedRealityHandRay), TypeGrouping.ByNamespaceFlat)]
+        private SystemType handRayType = null;
+
+        /// <summary>
+        /// The hand ray concrete type to use when raycasting for hand interaction.
+        /// </summary>
+        public SystemType HandRayType => handRayType;
+
+        [Header("Hand Physics")]
+
+        [SerializeField]
+        [Tooltip("If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.")]
+        private bool handPhysicsEnabled = false;
+
+        /// <summary>
+        /// If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.
+        /// </summary>
+        public bool HandPhysicsEnabled => handPhysicsEnabled;
+
+        [SerializeField]
+        [Tooltip("If set, hand colliders will be setup as triggers.")]
+        private bool useTriggers = false;
+
+        /// <summary>
+        /// If set, hand colliders will be setup as triggers.
+        /// </summary>
+        public bool UseTriggers => useTriggers;
+
+        [SerializeField]
+        [Tooltip("Set the bounds mode to use for calculating hand bounds.")]
+        private HandBoundsMode boundsMode = HandBoundsMode.Hand;
+
+        /// <summary>
+        /// Set the bounds mode to use for calculating hand bounds.
+        /// </summary>
+        public HandBoundsMode BoundsMode => boundsMode;
     }
 }

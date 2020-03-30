d3a3fe64082118a59feefa6f5c99a065535d4614
Stephen Hodgson <hodgson.designs@gmail.com>
2020-03-30 01:01:41 -0400

    Added IOS platform data provider

---
+++ XRTK-Core/Packages/com.xrtk.core/Services/PlatformSystem/Platforms/IOSPlatform.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.PlatformSystem;
+
+namespace XRTK.Services.PlatformSystem.Platforms
+{
+    /// <summary>
+    /// The IOS platform definition for the Mixed Reality Toolkit.
+    /// </summary>
+    public class IOSPlatform : BaseDataProvider, IMixedRealityPlatform
+    {
+        public IOSPlatform(string name, uint priority)
+            : base(name, priority)
+        {
+        }
+
+        public bool IsAvailable
+        {
+            get
+            {
+#if PLATFORM_IOS
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}

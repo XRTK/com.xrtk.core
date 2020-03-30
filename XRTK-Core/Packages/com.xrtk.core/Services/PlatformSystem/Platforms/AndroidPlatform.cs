d04927b40f34e29e4562011bfb141a6e23f4d937
Stephen Hodgson <hodgson.designs@gmail.com>
2020-03-30 00:25:48 -0400

    Added the android platform data provider

---
+++ XRTK-Core/Packages/com.xrtk.core/Services/PlatformSystem/Platforms/AndroidPlatform.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.PlatformSystem;
+
+namespace XRTK.Services.PlatformSystem.Platforms
+{
+    /// <summary>
+    /// The Android platform definition for the Mixed Reality Toolkit.
+    /// </summary>
+    public class AndroidPlatform : BaseDataProvider, IMixedRealityPlatform
+    {
+        public AndroidPlatform(string name, uint priority)
+            : base(name, priority)
+        {
+        }
+
+        public bool IsAvailable
+        {
+            get
+            {
+#if PLATFORM_ANDROID
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}

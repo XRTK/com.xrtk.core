e8ca3eee375fb173c30f9572125c6711e05c9a81
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-02 06:10:14 -0400

    added some docs

--- XRTK-Core/Packages/com.xrtk.core/Attributes/AxisConstraintAttribute.cs
+++ XRTK-Core/Packages/com.xrtk.core/Attributes/AxisConstraintAttribute.cs
@@ -2,10 +2,23 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
+using XRTK.Definitions.InputSystem;
 using XRTK.Definitions.Utilities;
 
 namespace XRTK.Attributes
 {
+    /// <summary>
+    /// Use this attribute to better define <see cref="MixedRealityInputAction"/>s.
+    /// This will filter the dropdown items to only display actions that match the <see cref="AxisConstraint"/>
+    /// </summary>
+    /// <example>
+    /// <para>Exclude a specific type from being selected:</para>
+    /// <code language="csharp"><![CDATA[
+    /// [SerializeField]
+    /// [AxisConstraint(AxisType.DualAxis)]
+    /// private MixedRealityInputAction testAction = MixedRealityInputAction.None;
+    /// ]]></code>
+    /// </example>
     public sealed class AxisConstraintAttribute : PropertyAttribute
     {
         public AxisType AxisConstraint { get; }

e1c65adfe343616dba36c98ddc417213b4e4d683
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-04 19:57:32 -0400

    Added BaseHandDatProvider and BaseHandDataProviderProfile

---
+++ XRTK-Core/Packages/com.xrtk.core/Definitions/Controllers/Hands/BaseHandDataProviderProfile.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace XRTK.Definitions.Controllers.Hands
+{
+    /// <summary>
+    /// Provides additional configuration options for hand data providers.
+    /// </summary>
+    public abstract class BaseHandDataProviderProfile : BaseMixedRealityControllerDataProviderProfile
+    {
+        [Header("General Settings")]
+
+        [SerializeField]
+        [Tooltip("If set, hand mesh data will be read and available for visualization. Disable for optimized performance.")]
+        private bool handMeshingEnabled = false;
+
+        /// <summary>
+        /// If set, hand mesh data will be read and available for visualization. Disable for optimized performance.
+        /// </summary>
+        public bool HandMeshingEnabled => handMeshingEnabled;
+
+        [Header("Hand Physics")]
+
+        [SerializeField]
+        [Tooltip("If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.")]
+        private bool handPhysicsEnabled = false;
+
+        /// <summary>
+        /// If set, hands will be setup with colliders and a rigidbody to work with Unity's physics system.
+        /// </summary>
+        public bool HandPhysicsEnabled => handPhysicsEnabled;
+
+        [SerializeField]
+        [Tooltip("If set, hand colliders will be setup as triggers.")]
+        private bool useTriggers = false;
+
+        /// <summary>
+        /// If set, hand colliders will be setup as triggers.
+        /// </summary>
+        public bool UseTriggers => useTriggers;
+
+        [SerializeField]
+        [Tooltip("Set the bounds mode to use for calculating hand bounds.")]
+        private HandBoundsMode boundsMode = HandBoundsMode.Hand;
+
+        /// <summary>
+        /// Set the bounds mode to use for calculating hand bounds.
+        /// </summary>
+        public HandBoundsMode BoundsMode => boundsMode;
+    }
+}

eed057e8dbdebd75dca3a803c10ba3b485ba4e78
Dino Fejzagic <dino.f@live.de>
2020-04-06 08:37:24 +0200

    Update IOSPlatform.cs

--- XRTK-Core/Packages/com.xrtk.core/Definitions/Platforms/IOSPlatform.cs
+++ XRTK-Core/Packages/com.xrtk.core/Definitions/Platforms/IOSPlatform.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+#if PLATFORM_IOS
+using UnityEngine;
+#endif
+
 namespace XRTK.Definitions.Platforms
 {
     /// <summary>

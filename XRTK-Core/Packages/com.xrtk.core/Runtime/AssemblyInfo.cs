5901773370216b00f368b55a3b4f2527f28d2407
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-26 21:58:24 -0400

    fixed editor asmdef and updated assembly info

--- XRTK-Core/Packages/com.xrtk.core/Runtime/AssemblyInfo.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 
 // Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
 
-[assembly: InternalsVisibleTo("XRTK.Inspectors")]
+[assembly: InternalsVisibleTo("XRTK.Editor")]
 [assembly: InternalsVisibleTo("XRTK.Tests")]
 [assembly: InternalsVisibleTo("XRTK.Utilities.Gltf.Importers")]
 [assembly: InternalsVisibleTo("XRTK.WindowsMixedReality")]

8bd7559136374f6d6ae804d9e311a4d60ae0ae6c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-27 02:41:13 -0400

    updated sdk and wmr package layout and profile location (#549)
    * updated sdk and wmr package layout and profile location
    
    * updated lumin references
    
    * updated lumin checkout
    
    * Moved all the folders into their correct places per UPM package layout guidelines
    
    * fixed editor asmdef and updated assembly info
    
    * updated submodules

---
+++ XRTK-Core/Packages/com.xrtk.core/Editor/Profiles/DiagnosticsSystem/MixedRealityDiagnosticsSystemProfileInspector.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using UnityEditor;
+using UnityEngine;
+using XRTK.Definitions.DiagnosticsSystem;
+using XRTK.Editor.Extensions;
+
+namespace XRTK.Editor.Profiles.DiagnosticsSystem
+{
+    [CustomEditor(typeof(MixedRealityDiagnosticsSystemProfile))]
+    public class MixedRealityDiagnosticsSystemProfileInspector : MixedRealityServiceProfileInspector
+    {
+        private SerializedProperty diagnosticsWindowPrefab;
+        private SerializedProperty showDiagnosticsWindowOnStart;
+
+        private readonly GUIContent generalSettingsFoldoutHeader = new GUIContent("General Settings");
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            diagnosticsWindowPrefab = serializedObject.FindProperty(nameof(diagnosticsWindowPrefab));
+            showDiagnosticsWindowOnStart = serializedObject.FindProperty(nameof(showDiagnosticsWindowOnStart));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderHeader("Diagnostics monitors system resources and performance inside an application during development.");
+
+            serializedObject.Update();
+
+            if (diagnosticsWindowPrefab.FoldoutWithBoldLabelPropertyField(generalSettingsFoldoutHeader))
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(showDiagnosticsWindowOnStart);
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ XRTK-Core/Packages/com.xrtk.core/Editor/Profiles/InputSystem/Controllers/Simulation/SimulatedHandControllerDataProviderProfileInspector.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using XRTK.Definitions.Controllers.Simulation.Hands;
+using XRTK.Editor.Extensions;
+
+namespace XRTK.Editor.Profiles.InputSystem.Controllers.Simulation
+{
+    [CustomEditor(typeof(SimulatedHandControllerDataProviderProfile))]
+    public class SimulatedHandControllerDataProviderProfileInspector : SimulatedControllerDataProviderProfileInspector
+    {
+        private static readonly GUIContent SimulatedHandSettingsFoldoutHeader = new GUIContent("Simulated Hand Tracking Settings");
+
+        private SerializedProperty handMeshingEnabled;
+        private SerializedProperty handPhysicsEnabled;
+        private SerializedProperty useTriggers;
+        private SerializedProperty boundsMode;
+
+        private SerializedProperty handPoseAnimationSpeed;
+        private SerializedProperty poseDefinitions;
+
+        private ReorderableList poseList;
+        private int currentlySelectedPoseElement;
+
+        private bool showSimulatedHandTrackingSettings = true;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            handMeshingEnabled = serializedObject.FindProperty(nameof(handMeshingEnabled));
+            handPhysicsEnabled = serializedObject.FindProperty(nameof(handPhysicsEnabled));
+            useTriggers = serializedObject.FindProperty(nameof(useTriggers));
+            boundsMode = serializedObject.FindProperty(nameof(boundsMode));
+
+            poseDefinitions = serializedObject.FindProperty(nameof(poseDefinitions));
+            handPoseAnimationSpeed = serializedObject.FindProperty(nameof(handPoseAnimationSpeed));
+
+            poseList = new ReorderableList(serializedObject, poseDefinitions, true, false, true, true)
+            {
+                elementHeight = EditorGUIUtility.singleLineHeight * 1.5f
+            };
+            poseList.drawHeaderCallback += DrawHeaderCallback;
+            poseList.drawElementCallback += DrawConfigurationOptionElement;
+            poseList.onAddCallback += OnConfigurationOptionAdded;
+            poseList.onRemoveCallback += OnConfigurationOptionRemoved;
+        }
+
+        private void DrawHeaderCallback(Rect rect)
+        {
+            EditorGUI.LabelField(rect, "Pose Definitions");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            EditorGUILayout.Space();
+
+            showSimulatedHandTrackingSettings = EditorGUILayoutExtensions.FoldoutWithBoldLabel(showSimulatedHandTrackingSettings, SimulatedHandSettingsFoldoutHeader, true);
+            if (showSimulatedHandTrackingSettings)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.LabelField("Hand Rendering Settings");
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(handMeshingEnabled);
+                EditorGUI.indentLevel--;
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Hand Physics Settings");
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(handPhysicsEnabled);
+                EditorGUILayout.PropertyField(useTriggers);
+                EditorGUILayout.PropertyField(boundsMode);
+                EditorGUILayout.Space();
+                EditorGUI.indentLevel--;
+                EditorGUILayout.LabelField("Simulated Poses");
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(handPoseAnimationSpeed);
+                poseList.DoLayoutList();
+                EditorGUI.indentLevel--;
+                EditorGUI.indentLevel--;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawConfigurationOptionElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            if (isFocused)
+            {
+                currentlySelectedPoseElement = index;
+            }
+
+            rect.height = EditorGUIUtility.singleLineHeight;
+            rect.y += 3;
+            var poseDataProperty = poseDefinitions.GetArrayElementAtIndex(index);
+            var selectedPoseData = EditorGUI.ObjectField(rect, poseDataProperty.objectReferenceValue, typeof(SimulatedHandControllerPoseData), false) as SimulatedHandControllerPoseData;
+
+            if (selectedPoseData != null)
+            {
+                selectedPoseData.ParentProfile = ThisProfile;
+            }
+
+            poseDataProperty.objectReferenceValue = selectedPoseData;
+        }
+
+        private void OnConfigurationOptionAdded(ReorderableList list)
+        {
+            poseDefinitions.arraySize += 1;
+            var index = poseDefinitions.arraySize - 1;
+
+            var mappingProfileProperty = poseDefinitions.GetArrayElementAtIndex(index);
+            mappingProfileProperty.objectReferenceValue = null;
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnConfigurationOptionRemoved(ReorderableList list)
+        {
+            if (currentlySelectedPoseElement >= 0)
+            {
+                poseDefinitions.DeleteArrayElementAtIndex(currentlySelectedPoseElement);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}

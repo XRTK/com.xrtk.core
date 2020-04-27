aa2c5adbcc4370da5fe280339050cca957e3c449
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-26 21:40:59 -0400

    Moved all the folders into their correct places per UPM package layout guidelines

---
+++ XRTK-Core/Packages/com.xrtk.core/Editor/PropertyDrawers/MixedRealityInputActionDropdown.cs
@@ -0,0 +1,145 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEditor;
+using UnityEngine;
+using XRTK.Definitions.InputSystem;
+using XRTK.Definitions.Utilities;
+using XRTK.Editor.Extensions;
+
+namespace XRTK.Editor.PropertyDrawers
+{
+    /// <summary>
+    /// Draws the <see cref="MixedRealityInputAction"/> property field in custom inspectors.
+    /// </summary>
+    /// <example>
+    /// <code language="csharp"><![CDATA[
+    /// public class CustomScript : Monobehaviour
+    /// {
+    ///     [SerializeField]
+    ///     private MixedRealityInputAction testAction = MixedRealityInputAction.None;
+    ///
+    ///     [SerializeField]
+    ///     [AxisConstraint(AxisType.DualAxis)]
+    ///     private MixedRealityInputAction test2Action = MixedRealityInputAction.None;
+    /// }
+    ///
+    /// [CustomEditor(typeof(CustomScript))]
+    /// public class CustomInspector : Editor
+    /// {
+    ///     private readonly MixedRealityInputActionDropdown inputActionDropdown = new MixedRealityInputActionDropdown();
+    ///
+    ///     private SerializedProperty testAction;
+    ///     private SerializedProperty test2Action;
+    ///
+    ///     private void OnEnable()
+    ///     {
+    ///         testAction = serializedObject.FindProperty(nameof(testAction));
+    ///         test2Action = serializedObject.FindProperty(nameof(test2Action));
+    ///     }
+    ///
+    ///     public override void OnInspectorGUI()
+    ///     {
+    ///         inputActionDropdown.OnGui(GUIContent.none, testAction);
+    ///         inputActionDropdown.OnGui(GUIContent.none, test2Action, AxisType.DualAxis);
+    ///
+    ///         // The same as:
+    ///         EditorGUILayout.PropertyField(testAction);
+    ///         EditorGUILayout.PropertyField(test2Action);
+    ///     }
+    /// }
+    /// ]]></code>
+    /// </example>
+    public class MixedRealityInputActionDropdown
+    {
+        private static readonly string DefaultGuidString = default(Guid).ToString("N");
+
+        private MixedRealityInputActionsProfile[] allInputActionProfiles;
+
+        public void OnGui(GUIContent content, SerializedProperty property, AxisType axisConstraintFilter = AxisType.None, params GUILayoutOption[] layoutOptions)
+        {
+            var rect = GUILayoutUtility.GetRect(content, "MiniPullDown", layoutOptions);
+            OnGui(rect, property, content, axisConstraintFilter);
+        }
+
+        public void OnGui(Rect rect, SerializedProperty property, GUIContent content, AxisType axisConstraintFilter = AxisType.None)
+        {
+            var label = EditorGUI.BeginProperty(rect, content, property);
+
+            var id = property.FindPropertyRelative("id");
+            var profileGuid = property.FindPropertyRelative("profileGuid");
+            var description = property.FindPropertyRelative("description");
+            var axisConstraint = property.FindPropertyRelative("axisConstraint");
+
+            if (string.IsNullOrWhiteSpace(profileGuid.stringValue))
+            {
+                profileGuid.stringValue = DefaultGuidString;
+            }
+
+            var currentAction = new MixedRealityInputAction(Guid.Parse(profileGuid.stringValue), (uint)id.intValue, description.stringValue, (AxisType)axisConstraint.intValue);
+
+            if (allInputActionProfiles == null)
+            {
+                allInputActionProfiles = ScriptableObjectExtensions.GetAllInstances<MixedRealityInputActionsProfile>();
+            }
+
+            var dropdownMenu = new GenericMenu { allowDuplicateNames = true };
+            dropdownMenu.AddItem(new GUIContent("None"), false, data => ResetAction(), null);
+
+            foreach (var inputActionProfile in allInputActionProfiles)
+            {
+                dropdownMenu.AddSeparator($"{inputActionProfile.name}/");
+
+                foreach (var inputAction in inputActionProfile.InputActions)
+                {
+                    if (axisConstraintFilter != AxisType.None &&
+                        axisConstraintFilter != inputAction.AxisConstraint)
+                    {
+                        if (inputAction == currentAction)
+                        {
+                            ResetAction();
+                        }
+
+                        continue;
+                    }
+
+                    dropdownMenu.AddItem(
+                        new GUIContent($"{inputActionProfile.name}/{inputAction.Description}"),
+                        inputAction == currentAction,
+                        OnItemSelect,
+                        null);
+
+                    void OnItemSelect(object _)
+                    {
+                        id.intValue = (int)inputAction.Id;
+                        description.stringValue = inputAction.Description;
+                        axisConstraint.intValue = (int)inputAction.AxisConstraint;
+                        profileGuid.stringValue = inputAction.ProfileGuid.ToString("N");
+                        property.serializedObject.ApplyModifiedProperties();
+                        GUI.changed = true;
+                    }
+                }
+            }
+
+            void ResetAction()
+            {
+                id.intValue = 0;
+                description.stringValue = "None";
+                axisConstraint.intValue = 0;
+                profileGuid.stringValue = DefaultGuidString;
+                property.serializedObject.ApplyModifiedProperties();
+                GUI.changed = true;
+            }
+
+            var prefix = EditorGUI.PrefixLabel(rect, label);
+
+            if (EditorGUI.DropdownButton(prefix, new GUIContent(description.stringValue), FocusType.Passive))
+            {
+                dropdownMenu.DropDown(prefix);
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ XRTK-Core/Packages/com.xrtk.core/Editor/Utilities/Lines/DataProviders/BezierDataProviderInspector.cs
@@ -0,0 +1,103 @@
+﻿using UnityEditor;
+using UnityEngine;
+using XRTK.Editor.Extensions;
+using XRTK.Utilities.Lines;
+
+namespace XRTK.Editor.Utilities.Lines.DataProviders
+{
+    [CustomEditor(typeof(BezierLineDataProvider))]
+    public class BezierDataProviderInspector : BaseMixedRealityLineDataProviderInspector
+    {
+        private const float HANDLE_SIZE_MODIFIER = 0.04f;
+        private const float PICK_SIZE_MODIFIER = 0.06f;
+
+        private SerializedProperty controlPoints;
+        private SerializedProperty inertia;
+        private SerializedProperty useLocalTangentPoints;
+
+        private int selectedHandleIndex = -1;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            controlPoints = serializedObject.FindProperty(nameof(controlPoints));
+            inertia = serializedObject.FindProperty(nameof(inertia));
+            useLocalTangentPoints = serializedObject.FindProperty(nameof(useLocalTangentPoints));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            // We always draw line points for bezier.
+            DrawLinePoints = true;
+            if (useLocalTangentPoints.FoldoutWithBoldLabelPropertyField(new GUIContent("Bezier Settings")))
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(controlPoints, true);
+                EditorGUILayout.PropertyField(inertia, true);
+                EditorGUI.indentLevel--;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected override void OnSceneGUI()
+        {
+            base.OnSceneGUI();
+
+            for (int i = 0; i < 4; i++)
+            {
+                serializedObject.Update();
+
+                bool isTangentHandle = i % 3 != 0;
+                bool isLastPoint = i == 3;
+
+                var controlPointPosition = LineData.GetPoint(i);
+                var controlPointProperty = controlPoints.FindPropertyRelative("point" + (i + 1));
+
+                // Draw our tangent lines
+                Handles.color = Color.gray;
+                if (i == 0)
+                {
+                    Handles.DrawLine(LineData.GetPoint(0), LineData.GetPoint(1));
+                }
+                else if (!isTangentHandle)
+                {
+                    Handles.DrawLine(LineData.GetPoint(i), LineData.GetPoint(i - 1));
+
+                    if (!isLastPoint)
+                    {
+                        Handles.DrawLine(LineData.GetPoint(i), LineData.GetPoint(i + 1));
+                    }
+                }
+
+                Handles.color = isTangentHandle ? Color.white : Color.green;
+                float handleSize = HandleUtility.GetHandleSize(controlPointPosition);
+
+                if (Handles.Button(controlPointPosition, Quaternion.identity, handleSize * HANDLE_SIZE_MODIFIER, handleSize * PICK_SIZE_MODIFIER, Handles.DotHandleCap))
+                {
+                    selectedHandleIndex = i;
+                }
+
+                // Draw our handles
+                if (Tools.current == Tool.Move && selectedHandleIndex == i)
+                {
+                    EditorGUI.BeginChangeCheck();
+
+                    var newTargetPosition = Handles.PositionHandle(controlPointPosition, Quaternion.identity);
+
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        Undo.RecordObject(LineData, "Change Bezier Point Position");
+                        LineData.SetPoint(i, newTargetPosition);
+                    }
+                }
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}

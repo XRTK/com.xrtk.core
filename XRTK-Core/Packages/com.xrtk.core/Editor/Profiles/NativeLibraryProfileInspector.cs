4dcf9989a1381bf9df0e8ba3b6da41fdbe6a9cd4
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-03 22:46:29 -0400

    updated profile inspector

--- XRTK-Core/Packages/com.xrtk.core/Editor/Profiles/NativeLibraryProfileInspector.cs
+++ XRTK-Core/Packages/com.xrtk.core/Editor/Profiles/NativeLibraryProfileInspector.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
 using UnityEditor;
-using XRTK.Editor.Profiles;
-using XRTK.Editor.Utilities;
 using XRTK.Services;
 
 namespace XRTK.Editor.Profiles
@@ -13,13 +11,7 @@ namespace XRTK.Editor.Profiles
     {
         public override void OnInspectorGUI()
         {
-            RenderHeader();
-
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Registered Native Data Providers Profile", EditorStyles.boldLabel);
-            EditorGUILayout.HelpBox("This profile defines any additional native data providers to register with the Mixed Reality Toolkit.\n\n" +
-                                    "Note: The order of the list determines the order these services get created.", MessageType.Info);
-            EditorGUILayout.Space();
+            RenderHeader("This profile defines any additional native data providers to register with the Mixed Reality Toolkit.");
 
             base.OnInspectorGUI();
         }

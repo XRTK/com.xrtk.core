770cd3602bf8156b479bd619b272150e824a0280
Dino Fejzagic <dino.f@live.de>
2020-05-09 08:29:29 +0200

    Update CollisionExtensions.cs

--- XRTK-Core/Packages/com.xrtk.core/Runtime/Extensions/CollisionExtensions.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Extensions/CollisionExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
@@ -15,15 +15,7 @@ namespace XRTK.Extensions.XRTK.Extensions
         /// <returns>True, if the collision is valid.</returns>
         public static bool IsValidCollision(this Collision collision, LayerMask[] prioritizedLayerMasks)
         {
-            for (int i = 0; i < prioritizedLayerMasks.Length; i++)
-            {
-                if (collision.gameObject.layer.IsInLayerMask(prioritizedLayerMasks[i]))
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return collision.collider.IsValidCollision(prioritizedLayerMasks);
         }
     }
 }

6ad1eddd867b41999acdab859e99fc0d957106a1
Dino Fejzagic <dino.f@live.de>
2020-05-09 10:01:18 +0200

    Feature/pni change requests (#577)
    * Update ColliderExtensions.cs
    
    * Update CollisionExtensions.cs

--- XRTK-Core/Packages/com.xrtk.core/Runtime/Extensions/ColliderExtensions.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Extensions/ColliderExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
@@ -37,6 +37,25 @@ namespace XRTK.Extensions.XRTK.Extensions
             {
                 positions[i] = positions[i].RotateAroundPoint(bounds.center, transform.rotation);
             }
+        }
+
+        /// <summary>
+        /// Checks if a collision is valid using a prioritized layer mask collection.
+        /// </summary>
+        /// <param name="collider"></param>
+        /// <param name="prioritizedLayerMasks"></param>
+        /// <returns>True, if the collision is valid.</returns>
+        public static bool IsValidCollision(this Collider collider, LayerMask[] prioritizedLayerMasks)
+        {
+            for (int i = 0; i < prioritizedLayerMasks.Length; i++)
+            {
+                if (collider.gameObject.layer.IsInLayerMask(prioritizedLayerMasks[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }

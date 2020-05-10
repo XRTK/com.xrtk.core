b7b6629257340f1eac092126b9785b173fa7ca58
Dino Fejzagic <dino.f@live.de>
2020-05-10 18:33:57 +0200

    Show button stations result

---
+++ XRTK-Core/Assets/HandExamples/Scripts/ButtonsStationResult.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ButtonsStationResult : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro resultText = null;
+
+    public void SetResult(string text)
+    {
+        resultText.text = $"You selected: {text}";
+    }
+}
---
+++ XRTK-Core/Assets/HandExamples/Scripts/DemoButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using XRTK.EventDatum.Input;
+using XRTK.Interfaces.InputSystem.Handlers;
+
+public class DemoButton : MonoBehaviour, IMixedRealityPointerHandler
+{
+    [SerializeField]
+    private Button.ButtonClickedEvent onClick = null;
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        onClick.Invoke();
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+
+    }
+}

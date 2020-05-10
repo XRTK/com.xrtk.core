63d88fb0cec873e4a0bdee77e9ccd8e6895c09c2
Dino Fejzagic <dino.f@live.de>
2020-05-10 19:55:40 +0200

    Implement set rendering mode station

---
+++ XRTK-Core/Assets/HandExamples/Scripts/SetRenderingMode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using XRTK.Interfaces.Providers.Controllers.Hands;
+using XRTK.Services;
+
+public class SetRenderingMode : MonoBehaviour
+{
+    public void SetNoneMode()
+    {
+        var providers = MixedRealityToolkit.GetActiveServices<IMixedRealityHandControllerDataProvider>();
+        for (int i = 0; i < providers.Count; i++)
+        {
+            providers[i].RenderingMode = XRTK.Definitions.Controllers.Hands.HandRenderingMode.None;
+        }
+    }
+
+    public void SetJointsMode()
+    {
+        var providers = MixedRealityToolkit.GetActiveServices<IMixedRealityHandControllerDataProvider>();
+        for (int i = 0; i < providers.Count; i++)
+        {
+            providers[i].RenderingMode = XRTK.Definitions.Controllers.Hands.HandRenderingMode.Joints;
+        }
+    }
+
+    public void SetMeshMode()
+    {
+        var providers = MixedRealityToolkit.GetActiveServices<IMixedRealityHandControllerDataProvider>();
+        for (int i = 0; i < providers.Count; i++)
+        {
+            providers[i].RenderingMode = XRTK.Definitions.Controllers.Hands.HandRenderingMode.Mesh;
+        }
+    }
+}

81d6c48d276eb321cb6c1d01beb6c60b52099c67
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-25 11:36:57 -0400

    Ensure project generation flags are set for 2019.3+

---
+++ XRTK-Core/Assets/XRTK.Editor/EditorSettingsCheck.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+
+namespace XRTK.Editor
+{
+    [InitializeOnLoad]
+    public static class EditorSettingsCheck
+    {
+        static EditorSettingsCheck()
+        {
+            if (EditorPrefs.GetInt("unity_project_generation_flag", 3) <= 3)
+            {
+                EditorPrefs.SetInt("unity_project_generation_flag", 3);
+            }
+        }
+    }
+}

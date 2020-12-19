83aa6b69d755802b7a79077f9dd953d01accd230
Dino Fejzagić <dino.f@live.de>
2020-12-19 21:11:35 +0100

    Define IMixedRealityTeleportValidationDataProvider

--- XRTK-Core/Packages/com.xrtk.core/Runtime/Interfaces/TeleportSystem/IMixedRealityTeleportValidationDataProvider.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Interfaces/TeleportSystem/IMixedRealityTeleportValidationDataProvider.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using UnityEngine;
+using XRTK.Interfaces.InputSystem;
+using XRTK.Services.Teleportation;
+
 namespace XRTK.Interfaces.TeleportSystem
 {
     /// <summary>
@@ -8,6 +12,13 @@ namespace XRTK.Interfaces.TeleportSystem
     /// </summary>
     public interface IMixedRealityTeleportValidationDataProvider : IMixedRealityTeleportDataProvider
     {
-
+        /// <summary>
+        /// Validates a <see cref="IPointerResult"/> and returns whether the <see cref="RaycastHit"/>
+        /// qualifies for teleporation.
+        /// </summary>
+        /// <param name="pointerResult">The <see cref="IPointerResult"/> to validate.</param>
+        /// <param name="teleportHotSpot"><see cref="IMixedRealityTeleportHotSpot"/> found at the target position, if any.</param>
+        /// <returns>The <see cref="TeleportValidationResult"/> for <paramref name="pointerResult"/>.</returns>
+        TeleportValidationResult IsValid(IPointerResult pointerResult, IMixedRealityTeleportHotSpot teleportHotSpot = null);
     }
 }

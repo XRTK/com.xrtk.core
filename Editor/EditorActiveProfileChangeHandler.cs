beb24f661d188240780ae6d6a8eb91813567573f
Dino Fejzagić <dino.f@live.de>
2021-01-15 01:31:11 +0100

    Fix NRE in EditorActiveProfileChangeHandler (#757)
    NREs were thrown in the handler if no active profile was set.

--- Editor/EditorActiveProfileChangeHandler.cs
+++ Editor/EditorActiveProfileChangeHandler.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
 using UnityEditor;
@@ -18,7 +18,7 @@ namespace XRTK.Editor
 
         private static void EditorApplication_hierarchyChanged()
         {
-            if (!MixedRealityToolkit.Instance.IsNull())
+            if (!(MixedRealityToolkit.Instance.IsNull() || MixedRealityToolkit.Instance.ActiveProfile.IsNull()))
             {
                 if (MixedRealityToolkit.Instance.ActiveProfile.IsInputSystemEnabled &&
                     Utilities.InputMappingAxisUtility.CheckUnityInputManagerMappings(Utilities.ControllerMappingUtilities.UnityInputManagerAxes))

147c93e01bc6af5f9d05008f85c6e62cd52c7543
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2021-05-16 14:30:04 +0100

    Adding compiler definitions for platforms

---
+++ XRTK-Core/Packages/com.xrtk.core/Editor/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define XRTK_EDITOR
--- XRTK-Core/Packages/com.xrtk.core/Runtime/AssemblyInfo.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+#define XRTK
+
 using System.Runtime.CompilerServices;
 
 // Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.

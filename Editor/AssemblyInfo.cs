dcd00a72b67fa37ede514aefb5eae3bc38224bde
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-16 14:52:45 -0400

    auto update assembly version based on package.json info (#840)
    * auto update assembly version based on package.json info
    
    * Added Modules property to the MixedRealityToolkit to get a list of the currently loaded modules in runtime and their versions
    
    * made preprocessor internal

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_EDITOR
 
 using System.Reflection;
 
+[assembly: AssemblyVersion("0.2.15")]
 [assembly: AssemblyTitle("com.xrtk.core.editor")]
 [assembly: AssemblyCompany("XRTK")]
 [assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]

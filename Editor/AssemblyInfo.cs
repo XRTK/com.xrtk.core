5b7377bcdbd6c1ac299b4b3bb3e4c85fbd08b619
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2021-05-16 16:49:32 +0100

    Adding compiler definitions for platforms (#839)
    * Adding compiler definitions for platforms
    
    * updated submodules
    
    * updated gltf submodule
    
    * added more assembly info
    
    Co-authored-by: Stephen Hodgson <hodgson.designs@gmail.com>

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define XRTK_EDITOR
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("com.xrtk.core.editor")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,13 +1,18 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+#define XRTK
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
+[assembly: AssemblyTitle("com.xrtk.core")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
 
+// Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
 [assembly: InternalsVisibleTo("XRTK.Editor")]
 [assembly: InternalsVisibleTo("XRTK.Tests")]
-[assembly: InternalsVisibleTo("XRTK.Utilities.Gltf.Importers")]
 [assembly: InternalsVisibleTo("XRTK.WindowsMixedReality")]
 [assembly: InternalsVisibleTo("XRTK.WindowsMixedReality.Player")]
 [assembly: InternalsVisibleTo("XRTK.Oculus")]

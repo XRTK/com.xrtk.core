0be613c15607d84311ee845528d938b6e37948d2
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-28 06:49:11 -0700

    Updated the AssemblyDefinitionPreProcessor to create assembly info if they didn't exist before (#857)
    Updates all assembly infos in the package instread of just to two predefinied ones

--- Tests/AssemblyInfo.cs
+++ Tests/AssemblyInfo.cs
@@ -2,12 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("0.2.18")]
-[assembly: AssemblyTitle("com.xrtk.core.tests.services")]
+[assembly: AssemblyTitle("com.xrtk.tests")]
 [assembly: AssemblyCompany("XRTK")]
 [assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
-
-// Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
-[assembly: InternalsVisibleTo("XRTK.Tests")]

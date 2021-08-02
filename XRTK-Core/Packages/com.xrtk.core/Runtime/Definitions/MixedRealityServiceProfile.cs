d7cbdeeb65603834c6615765861503287851f8b2
Simon Jackson <darkside@zenithmoon.com>
2021-08-02 17:00:23 +0100

    Restored Registered Service Profile inspector and renamed to MixedRealityServiceProfile

---
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Definitions/MixedRealityServiceProfile.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using UnityEngine;
+using XRTK.Definitions.Utilities;
+using XRTK.Interfaces;
+
+namespace XRTK.Definitions
+{
+    [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Base Service Profile", fileName = "MixedRealityServiceProfile", order = (int)CreateProfileMenuItemIndices.ServiceProviders)]
+    public class MixedRealityServiceProfile : BaseMixedRealityServiceProfile<IMixedRealityService>
+    { }
+}

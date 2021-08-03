b69b897e259e82e7c32f32e23dea6ec66ccf3e01
Dino Fejzagic <dino.f@live.de>
2021-08-03 07:55:06 +0200

    Improve provider docs and add input source to interface as an requirement

--- XRTK-Core/Packages/com.xrtk.core/Runtime/Interfaces/LocomotionSystem/ITeleportTargetProvider.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Interfaces/LocomotionSystem/ITeleportTargetProvider.cs
@@ -1,26 +1,37 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using XRTK.Interfaces.InputSystem;
 using XRTK.Services.LocomotionSystem;
 
 namespace XRTK.Interfaces.LocomotionSystem
 {
     /// <summary>
     /// Interface definition for components and services providing teleportation targets for
-    /// <see cref="ITeleportLocomotionProvider"/>s.
+    /// <see cref="ITeleportLocomotionProvider"/>s. Whenever an <see cref="ITeleportLocomotionProvider"/>
+    /// requests a teleportation target, the target provider will answer the request, if both share
+    /// the same <see cref="IMixedRealityInputSource"/> connectinon.
     /// </summary>
     public interface ITeleportTargetProvider
     {
         /// <summary>
         /// Gets the <see cref="ITeleportLocomotionProvider"/> that is currently requesting a teleport target
-        /// from this target provider, if any.
+        /// from this provider, if any.
         /// </summary>
         ITeleportLocomotionProvider RequestingLocomotionProvider { get; }
 
         /// <summary>
-        /// The result from the last raycast.
+        /// Gets the <see cref="IMixedRealityInputSource"/> this provider is
+        /// connected with. Each <see cref="ITeleportTargetProvider"/> must be
+        /// assigned an input source so targets can be mapped to the
+        /// <see cref="RequestingLocomotionProvider"/>'s input source.
+        /// </summary>
+        IMixedRealityInputSource InputSource { get; }
+
+        /// <summary>
+        /// Gets the validation result for the current target location. The provider can only
+        /// provide valid teleportation results to <see cref="ITeleportLocomotionProvider"/>s.
         /// </summary>
         TeleportValidationResult ValidationResult { get; }
     }
 }
-

6f19a20a0c9ffd7c548f62c4d5851552ee9efad2
Stephen Hodgson <hodgson.designs@gmail.com>
2022-08-30 12:14:17 -0400

    removed unnecessary guid from touch screen controller data provider

--- XRTK-Core/Packages/com.xrtk.core/Runtime/Definitions/Controllers/UnityInput/TouchScreenControllerDataProviderProfile.cs
+++ XRTK-Core/Packages/com.xrtk.core/Runtime/Definitions/Controllers/UnityInput/TouchScreenControllerDataProviderProfile.cs
@@ -6,7 +6,6 @@ using XRTK.Providers.Controllers.UnityInput;
 
 namespace XRTK.Definitions.Controllers.UnityInput.Profiles
 {
-    [System.Runtime.InteropServices.Guid("344B09FD-88CA-4C4D-BD90-0F406771CF3D")]
     public class TouchScreenControllerDataProviderProfile : BaseMixedRealityControllerDataProviderProfile
     {
         public override ControllerDefinition[] GetDefaultControllerOptions()
